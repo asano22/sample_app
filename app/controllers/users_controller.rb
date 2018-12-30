34be621039a8125e84c18544b6889cae13c4619a
t-asano <t-asano@xseed.co.jp>
2018-12-30 22:39:28 +0900

    アカウント有効機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,14 @@ class UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.paginate(page: params[:page])
+    # @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
     # debugger
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
@@ -21,9 +23,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      # log_in @user
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to @user
+
+      # UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :account_activations, only: [:edit]
 end
